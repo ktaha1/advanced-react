27a8bdaa1f82578ade724d6e794ec60b83400a64
Taha <itaha.kamil@gmail.com>
2022-01-20 22:48:10 +0100

    add useEffect fetch-data completed

--- src/useEffect/fetchData.js
+++ src/useEffect/fetchData.js
@@ -13,13 +13,26 @@ const FetchData = () => {
 
   useEffect(() => {
     fetchUser();
-  });
+  }, []);
 
   return (
     <>
       <h3>Github Users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
-
 export default FetchData;
